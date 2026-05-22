08d0261657fb904c1e5c3ceb15a8f7a1b4b25ebc
khy1121 <rlagjsdud3@hansung.ac.kr>
2026-05-22 20:02:44 +0900

    feat: 온보딩 스플래시 페이지 구현
    - Figma 1493-3626 기준으로 스플래시 화면 구현
    - onboardMain.svg 배경 이미지 추가
    - 금색 오버레이, Tropi Land 로고, 서브타이틀, 글래스 버튼 구현
    - CharacterPage 패턴(inner 래퍼, --vh 기반 배치)으로 비-852px 화면 대응
    - body.splash-page-active 클래스로 전체화면 레이아웃 처리

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../../constants/routes'
 import './SplashPage.scss'
@@ -6,12 +6,26 @@ import './SplashPage.scss'
 export default function SplashPage() {
   const navigate = useNavigate()
 
+  useEffect(() => {
+    document.body.classList.add('splash-page-active')
+
+    return () => {
+      document.body.classList.remove('splash-page-active')
+    }
+  }, [])
+
   return (
     <div className="splash">
-      <div className="splash__logo">나독</div>
-      <button className="splash__btn" onClick={() => navigate(ROUTES.SIGNUP)}>
-        시작하기
-      </button>
+      <div className="splash__overlay" />
+      <div className="splash__inner">
+        <div className="splash__logo-group">
+          <h1 className="splash__title">DOK</h1>
+          <p className="splash__subtitle">펼치는 순간, 나를 다독이다</p>
+        </div>
+        <button className="splash__btn" onClick={() => navigate(ROUTES.LOGIN)}>
+          시작하기
+        </button>
+      </div>
     </div>
   )
 }
